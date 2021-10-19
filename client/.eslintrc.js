567d0ccd44a5a87d829055c294829f04af8353af
UP2012320 <UP2012320@myport.ac.uk>
2021-10-19 15:45:25 +0100

    Test commit

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,12 +12,10 @@ module.exports = {
   parserOptions: {
     project: path.resolve(__dirname, './tsconfig.json'),
     tsconfigRootDir: __dirname,
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
