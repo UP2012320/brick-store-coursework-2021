4980dea0ebc9363686faf98958e8bd01bf206306
UP2012320 <UP2012320@myport.ac.uk>
2021-10-21 01:39:35 +0100

    Implement componentElement.ts, a fluent way to structure the appending of elements during builds

---
+++ client/src/scripts/framework/componentElement.ts
@@ -0,0 +1,44 @@
+class ComponentElement {
+  element: Element;
+  parent?: ComponentElement;
+  children: ComponentElement[] = [];
+
+  constructor(element: Element, parent?: ComponentElement) {
+    this.element = element;
+    this.parent = parent;
+  }
+
+  static create(element: Element) {
+    return new ComponentElement(element);
+  }
+
+  then(element: Element) {
+    const e = new ComponentElement(element, this);
+    this.children.push(e);
+    return e;
+  }
+
+  up() {
+    return this.parent ? this.parent : this;
+  }
+
+  private build() {
+    let child: ComponentElement | undefined;
+
+    while ((child = this.children.shift())) {
+      this.element.appendChild(child.build());
+    }
+
+    return this.element;
+  }
+
+  end() {
+    let parent = this.parent;
+
+    while (parent?.parent) {
+      parent = parent?.parent;
+    }
+
+    return parent?.build();
+  }
+}
