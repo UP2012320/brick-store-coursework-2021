d349024e770393f260c43f3af5a340cc6ae0ab4d
UP2012320 <UP2012320@myport.ac.uk>
2021-10-28 01:40:58 +0100

    Configure Jest to ignore cypress

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,8 +4,9 @@ const {pathsToModuleNameMapper} = require('ts-jest/utils');
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const {compilerOptions} = require('./tsconfig.json');
+const path = require('path');
 
-let mapping = pathsToModuleNameMapper(compilerOptions.paths, { prefix: `${__dirname}/src` });
+let mapping = pathsToModuleNameMapper(compilerOptions.paths, {prefix: path.join(__dirname, 'src')});
 mapping = {...mapping, ...{'^(\\.{1,2}/.*)\\.js$': '$1'}};
 
 module.exports = {
@@ -14,14 +15,15 @@ module.exports = {
   moduleNameMapper: mapping,
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
   extensionsToTreatAsEsm: ['.ts'],
   collectCoverage: true,
   globals: {
     'ts-jest': {
-      useESM: true
-    }
-  }
+      useESM: true,
+    },
+  },
+  testPathIgnorePatterns: [path.join(__dirname, 'cypress')],
 };
