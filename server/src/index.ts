cead3ec5ad70dd318081767712a73f24d3dc33e8
UP2012320 <UP2012320@myport.ac.uk>
2021-12-03 13:34:07 +0000

    Device sync

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,15 @@ import fastifySensible from 'fastify-sensible';
 import fastifyStatic from 'fastify-static';
 import path from 'path';
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const PG_CONNECTION_STRING =
+  process.env.PG_CONNECTION_STRING ||
+  'postgresql://brick_store_user:y*_dwYx7CkTbaQQ!@localhost:5432/brick_store_coursework';
 const app = fastify();
 
 app.register(fastifyHelmet, {contentSecurityPolicy: false});
 app.register(fastifyPostgres, {
-  connectionString: '',
+  connectionString: PG_CONNECTION_STRING,
 });
 app.register(fastifySensible);
 app.register(fastifyStatic, {
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,11 +1,74 @@
 import {FastifyInstance, FastifyServerOptions} from 'fastify';
 
+interface SearchBody {
+  query: string;
+  colour?: string;
+  size?: string;
+  type?: string;
+  price?: {
+    min?: number;
+    max?: number;
+  };
+  in_stock?: boolean;
+  page?: number;
+  sort?: string;
+}
+
 export default function api(
   fastify: FastifyInstance,
   opts: FastifyServerOptions,
   done: (err?: Error) => void,
 ) {
-  fastify.get<{Querystring: {sort: string}}>('/', async (request, reply) => {
+  fastify.post<{Body: SearchBody}>('/search', async (request, reply) => {
+    const body = request.body;
+
+    if (!body || !body.query) {
+      reply.badRequest('Invalid JSON body');
+      return;
+    }
+
+    let pg;
+
+    try {
+      pg = await fastify.pg.connect();
+    } catch (e) {
+      console.debug(e);
+      reply.internalServerError();
+      return;
+    }
+
+    try {
+      const f = await pg.query(
+        `SELECT *
+         FROM inventory
+         WHERE item_name ILIKE '%' || $1 || '%'
+           AND colour ILIKE '%' || $2 || '%'
+           AND size ILIKE '%' || $3 || '%'
+           AND type ILIKE '%' || $4 || '%'
+           AND (price >= $5 AND price <= $6)
+           AND stock >= $7
+         ORDER BY $8
+         OFFSET $9 LIMIT 50`,
+        [
+          body.query,
+          body.colour ?? '',
+          body.size ?? '',
+          body.type ?? '',
+          body.price?.min ?? 0,
+          body.price?.max ?? Number.MAX_VALUE,
+          body.in_stock ? 1 : 0,
+          body.sort ?? 'item_name',
+          50 * (body.page ?? 0),
+        ],
+      );
+
+      console.debug(f.rows);
+    } catch (e) {
+      console.debug(e);
+    }
+
+    console.debug(body);
+
     return 'Hello World!';
   });
 
