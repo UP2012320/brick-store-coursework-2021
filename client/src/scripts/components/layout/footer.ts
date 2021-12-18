776a476ec194949c4742d3bdeea52c1007687282
UP2012320 <UP2012320@myport.ac.uk>
2021-12-18 19:02:47 +0000

    Add text to footer for testing

--- client/src/scripts/components/layout/footer.ts
+++ client/src/scripts/components/layout/footer.ts
@@ -1,9 +1,14 @@
+import html from 'Scripts/htmlTemplate';
 import {createElement} from 'Scripts/uiUtils';
 // eslint-disable-next-line import/no-unassigned-import
-import 'Styles/components/footer.scss';
+import 'Styles/components/footer.module.scss';
 
 export default function createFooter () {
   const footer = createElement('footer');
 
-  return footer;
+  return html`
+    <${footer}>
+      <${createElement('p', {textContent: 'footer'})}/>
+    </footer>
+  `;
 }
