c2314d0fb28398c4b0ff7a0de9db873df8e4a6ce
UP2012320 <UP2012320@myport.ac.uk>
2022-02-05 00:13:58 +0000

    Improve searchInventory.sql to mitigate sql injections

--- server/sqlFunctions/searchInventory.sql
+++ server/sqlFunctions/searchInventory.sql
@@ -1,20 +1,27 @@
-CREATE FUNCTION search_inventory(_query text, _colour integer DEFAULT NULL, _type integer DEFAULT NULL,
-                                 _minPrice numeric DEFAULT NULL, _maxPrice numeric DEFAULT NULL,
-                                 _stock numeric DEFAULT NULL, _offset integer DEFAULT 0,
-                                 _limit integer DEFAULT 20) RETURNS SETOF inventory
+CREATE OR REPLACE FUNCTION search_inventory(_query text, _colour integer DEFAULT NULL, _type integer DEFAULT NULL,
+                                            _minPrice numeric DEFAULT NULL, _maxPrice numeric DEFAULT NULL,
+                                            _stock numeric DEFAULT NULL, _offset integer DEFAULT 0,
+                                            _limit integer DEFAULT 20) RETURNS SETOF inventory
   LANGUAGE plpgsql
 AS
 $$
 BEGIN
-  RETURN QUERY
-    SELECT *
-    FROM inventory
-    WHERE (_query IS NULL OR item_name ILIKE '%' || _query || '%')
-      AND (_colour IS NULL OR colour = _colour)
-      AND (_type IS NULL OR type = _type)
-      AND ((_minPrice IS NULL OR price >= _minPrice) AND (_maxPrice IS NULL OR price <= _maxPrice))
-      AND (_stock IS NULL OR stock >= _stock)
-    OFFSET _offset LIMIT _limit;
+  IF _offset IS NULL THEN
+    _offset = 0;
+  END IF;
+
+  IF _limit IS NULL THEN
+    _limit = 20;
+  END IF;
+
+  RETURN QUERY EXECUTE 'SELECT * FROM inventory ' ||
+                       'WHERE ($1 IS NULL OR item_name ILIKE ' || QUOTE_LITERAL('%' || _query || '%') || ') AND ' ||
+                       '($2 IS NULL OR colour = $2) AND ' ||
+                       '($3 IS NULL OR type = $3) AND ' ||
+                       '(($4 IS NULL OR price >= $4) AND ($5 IS NULL OR price <= $5)) AND ' ||
+                       '($6 IS NULL OR stock >= $6)' ||
+                       'OFFSET $7 LIMIT $8'
+    USING _query, _colour, _type, _minPrice, _maxPrice, _stock, _offset, _limit;
 END
 $$;
 
