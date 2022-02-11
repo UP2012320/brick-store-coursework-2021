db2bca4f76a42c539898fe6a98071d3faf2b94ac
UP2012320 <UP2012320@myport.ac.uk>
2022-02-11 18:10:16 +0000

    Update search sql

--- server/sqlFunctions/searchInventory.sql
+++ server/sqlFunctions/searchInventory.sql
@@ -2,7 +2,19 @@ CREATE OR REPLACE FUNCTION search_inventory(_query text, _colour integer DEFAULT
                                             _minPrice numeric DEFAULT NULL, _maxPrice numeric DEFAULT NULL,
                                             _stock numeric DEFAULT NULL, _sort text DEFAULT 'item_name',
                                             _sortDirection integer DEFAULT 0, _offset integer DEFAULT 0,
-                                            _limit integer DEFAULT 20) RETURNS SETOF inventory
+                                            _limit integer DEFAULT 20)
+  RETURNS TABLE (
+                  id             varchar(11),
+                  name           varchar(500),
+                  slug           varchar(500),
+                  description    varchar(2500),
+                  price          numeric,
+                  stock          integer,
+                  discount       numeric,
+                  discount_price numeric,
+                  colour         varchar(100),
+                  type           varchar(100)
+                )
   LANGUAGE plpgsql
 AS
 $$
@@ -31,7 +43,9 @@ BEGIN
     _sortDirection = 0;
   END IF;
 
-  RETURN QUERY EXECUTE 'SELECT * FROM inventory ' ||
+  RETURN QUERY EXECUTE 'SELECT inventory_id AS "id", item_name AS "name", slug, description, price, stock, discount, ROUND(COALESCE(discount, 1) * price, 2) AS "discount_price", bc.colour_name AS "colour", bt.type_name FROM inventory ' ||
+                       'JOIN brick_colours bc ON bc.colour_id = inventory.colour ' ||
+                       'JOIN brick_types bt ON bt.type_id = inventory.type ' ||
                        'WHERE ($1 IS NULL OR item_name ILIKE ' || QUOTE_LITERAL(_query) || ') AND ' ||
                        '($2 IS NULL OR colour = $2) AND ' ||
                        '($3 IS NULL OR type = $3) AND ' ||
@@ -43,5 +57,3 @@ BEGIN
     USING _query, _colour, _type, _minPrice, _maxPrice, _stock, _offset, _limit;
 END
 $$;
-
-ALTER FUNCTION search_inventory(text, integer, integer, numeric, numeric, numeric, text, integer, integer, integer) OWNER TO brick_store_user;
