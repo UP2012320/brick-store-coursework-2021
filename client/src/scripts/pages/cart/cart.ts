f5b865da360123bbcedc336f329650498f88c07e
UP2012320 <UP2012320@myport.ac.uk>
2022-03-12 11:17:39 +0000

    Start work on cart page

--- client/src/scripts/pages/cart/cart.ts
+++ client/src/scripts/pages/cart/cart.ts
@@ -1,5 +1,19 @@
 import htmlx from 'Scripts/htmlX';
+import {createElementWithStyles} from 'Scripts/uiUtils';
+import cartStyles from './cart.module.scss';
 
 export default function createCart () {
-  return htmlx``;
+  const cartScrollContainer = createElementWithStyles('section', undefined, cartStyles.cartScrollContainer);
+  const cartContainer = createElementWithStyles('div', undefined, cartStyles.cartContainer);
+  const cart = createElementWithStyles('div', undefined, cartStyles.cartRow);
+
+  return htmlx`
+  <${cartScrollContainer}>
+    <${cartContainer}>
+      <${cart}>
+
+      </cart>
+    </cartContainer>
+  </cartScrollContainer>
+  `;
 }
