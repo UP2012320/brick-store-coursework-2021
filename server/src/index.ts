19739deec08e11247afe521f80cc33dd38fba1bd
UP2012320 <UP2012320@myport.ac.uk>
2022-04-13 19:53:42 +0100

    Rollback serverless changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,47 @@
-import type {FastifyPluginAsync} from 'fastify';
+import path from 'node:path';
+import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
+import fastifyHelmet from 'fastify-helmet';
+import fastifyPostgres from 'fastify-postgres';
+import fastifySensible from 'fastify-sensible';
+import fastifyStatic from 'fastify-static';
+import config from './config.js';
 import api from './routes/api.js';
 
-const base: FastifyPluginAsync = async (fastify, options) => {
-  fastify.get('*', async (request, reply) => {
+const ADDRESS = config.address;
+const PORT = config.port;
+const PG_USER = config.pgUser;
+const PG_PASSWORD = config.pgPass;
+const PG_PORT = config.pgPort;
+const PG_ADDRESS = config.pgAddress;
+const PG_CONNECTION_STRING = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_ADDRESS}:${PG_PORT}/brick_store_coursework`;
+const app = fastify();
+
+(async () => {
+  await app.register(fastifyHelmet, {contentSecurityPolicy: false});
+  await app.register(fastifyCors, {origin: config.dev ? 'http://localhost:8080' : false});
+  await app.register(fastifyPostgres, {
+    connectionString: PG_CONNECTION_STRING,
+  });
+
+  await app.register(fastifySensible);
+  await app.register(fastifyStatic, {
+    prefix: '/public/',
+    root: path.join(__dirname, '../../dist/public'),
+  });
+
+  app.get('*', async (request, reply) => {
     await reply.sendFile('index.html');
   });
 
-  await fastify.register(api, {prefix: '/api/v1'});
-};
+  await app.register(api, {prefix: '/api/v1'});
 
-export default base;
+  try {
+    await app.listen(PORT, ADDRESS);
+  } catch (error) {
+    console.debug(error);
+    return;
+  }
+
+  console.debug(`Listening at http://${ADDRESS}:${PORT}`);
+})();
