9387643e5b89044acea14d17c60791b1ca4fe664
UP2012320 <UP2012320@myport.ac.uk>
2022-04-17 17:08:26 +0100

    Add table header row

--- client/src/scripts/components/inventoryTable/inventoryTable.ts
+++ client/src/scripts/components/inventoryTable/inventoryTable.ts
@@ -1,5 +1,60 @@
 import {nameof} from 'Scripts/helpers';
+import htmlx from 'Scripts/htmlX';
+import {createElement, createElementWithStyles, createKeyedContainer} from 'Scripts/uiUtils';
+import inventoryTableStyles from './inventoryTable.module.scss';
 
 const key = nameof(createInventoryTable);
 
-export default function createInventoryTable () {}
+export default function createInventoryTable () {
+  const container = createKeyedContainer('div', key, undefined, inventoryTableStyles.container);
+
+  const header = createElementWithStyles('header', undefined, inventoryTableStyles.header);
+
+  const headerName = createElementWithStyles('button', undefined, inventoryTableStyles.headerName, inventoryTableStyles.selected);
+  const headerNameText = createElement('p', {
+    textContent: 'Name',
+  });
+  const headerNameSortArrow = createElementWithStyles('i', undefined, inventoryTableStyles.biCaretDownFill);
+
+  const headerPrice = createElementWithStyles('button', undefined, inventoryTableStyles.headerPrice);
+  const headerPriceText = createElement('p', {
+    textContent: 'Price',
+  });
+  const headerPriceSortArrow = createElementWithStyles('i', undefined, inventoryTableStyles.biCaretDownFill);
+
+  const headerSearch = createElementWithStyles('div', undefined, inventoryTableStyles.headerSearch);
+  const headerSearchBox = createElementWithStyles('input', {
+    placeholder: 'Search',
+  }, inventoryTableStyles.headerSearch);
+
+  const headerAdd = createElementWithStyles('div', undefined, inventoryTableStyles.headerAdd);
+  const headerAddButton = createElementWithStyles('button', {
+    textContent: 'Add',
+  }, inventoryTableStyles.actionButtonNoBorder);
+
+  const body = createElementWithStyles('section', undefined, inventoryTableStyles.body);
+
+  return htmlx`
+  <${container}>
+    <${header}>
+      <${headerName}>
+        <${headerNameText}/>
+        <${headerNameSortArrow}/>
+      </${headerName}>
+      <${headerPrice}>
+        <${headerPriceText}/>
+        <${headerPriceSortArrow}/>
+      </${headerPrice}>
+      <${headerSearch}>
+        <${headerSearchBox}/>
+      </${headerSearch}>
+      <${headerAdd}>
+        <${headerAddButton}/>
+      </${headerAdd}>
+    </header>
+    <${body}>
+
+    </body>
+  </container>
+  `;
+}
--- client/src/scripts/pages/staff/manageProducts/manageProducts.ts
+++ client/src/scripts/pages/staff/manageProducts/manageProducts.ts
@@ -1,11 +1,13 @@
+import createInventoryTable from 'Scripts/components/inventoryTable/inventoryTable';
 import {nameof} from 'Scripts/helpers';
 import useSearch from 'Scripts/hooks/useSearch';
-import htmlx from 'Scripts/htmlX';
 
 const key = nameof(createManageProducts);
 
 export default function createManageProducts () {
   const [searchResults, setSearchState, pageNumber, noMoreResults, requestError] = useSearch(key);
 
-  return [htmlx``];
+  const inventoryTable = createInventoryTable();
+
+  return [inventoryTable];
 }
