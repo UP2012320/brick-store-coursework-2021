f897da08b19de1311417eb33ed7f553586560d72
UP2012320 <UP2012320@myport.ac.uk>
2022-04-23 03:14:22 +0100

    Make staff sidebar collapsable

--- client/src/scripts/components/layout/staff/sidebar.ts
+++ client/src/scripts/components/layout/staff/sidebar.ts
@@ -1,6 +1,10 @@
+import {nameof} from 'Scripts/helpers';
+import {useState} from 'Scripts/hooks/useState';
 import htmlx from 'Scripts/htmlX';
 import {createElementWithStyles, registerLinkClickHandler} from 'Scripts/uiUtils';
 import sidebarStyles from './sidebar.module.scss';
+
+const key = nameof(createSidebar);
 
 export interface SidebarProps {
   options: Array<{
@@ -11,12 +15,30 @@ export interface SidebarProps {
 }
 
 export default function createSidebar (props: SidebarProps) {
+  const [collapsed, setCollapsed] = useState(key, false);
+
   const container = createElementWithStyles('div', undefined, sidebarStyles.sidebarContainer);
 
   const titleContainer = createElementWithStyles('div', undefined, sidebarStyles.sidebarTitleContainer);
   const title = createElementWithStyles('h1', {
     textContent: props.title,
   }, sidebarStyles.sidebarTitle);
+
+  const onCollapse = () => {
+    setCollapsed((previous) => !previous);
+  };
+
+  const collapseButton = createElementWithStyles('i', {
+    onclick: () => onCollapse(),
+  }, sidebarStyles.biCaretLeftFill);
+
+  if (collapsed) {
+    container.classList.add(sidebarStyles.collapsed);
+    collapseButton.classList.add(sidebarStyles.toggled);
+  } else {
+    container.classList.remove(sidebarStyles.collapsed);
+    collapseButton.classList.remove(sidebarStyles.toggled);
+  }
 
   const rowsContainer = createElementWithStyles('div', undefined, sidebarStyles.rowsContainer);
 
@@ -36,6 +58,7 @@ export default function createSidebar (props: SidebarProps) {
     <${container}>
      <${titleContainer}>
         <${title}/>
+        <${collapseButton}/>
      </${titleContainer}>
      <${rowsContainer}>
         <${rows}/>
